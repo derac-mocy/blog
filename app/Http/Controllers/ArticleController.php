51a83d86404f2534c433d1fa6e6b81b9b40a5d16
derac-mocy <heinhtetwin.449@gmail.com>
2021-01-06 22:36:23 +0630

    Generating dummy data in articles table by using Mode,  Factory & Seeder

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {

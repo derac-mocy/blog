ea3d4f64c3296ee7b8ddc38b582e0e48ff88b169
derac-mocy <heinhtetwin.449@gmail.com>
2021-01-06 17:32:55 +0630

    Testing View with Controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function index() {
-        return "Article Controller - Article List";
+        $data = [
+            [ "id" => 1, "title" => "First Article"],
+            [ "id" => 2, "title" => "Second Article"],
+        ];
+        return view('articles.index', ['articles' => $data]);
     }
 
     public function detail($id) {
---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Laravel 8 Blog Application</title>
+</head>
+<body>
+    <h1>Article list</h1>
+    <ul>
+        @foreach($articles as $article)
+        <li>{{ $article['title'] }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>

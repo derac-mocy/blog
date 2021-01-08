6ec32d3c76c784a4c0716836c7b37bedf63b08fc
derac-mocy <heinhtetwin.449@gmail.com>
2021-01-08 13:27:28 +0630

    Assigning Table Relationships & Retrieving data from tables

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -6,7 +6,8 @@
             <div class="card-body">
                 <h5 class="card-title">{{ $article->title }}</h5>
                 <div class="card-subtitle mb-2 text-muted small">
-                    {{ $article->created_at->diffForHumans() }}    
+                    {{ $article->created_at->diffForHumans() }},
+                    Category: <b>{{ $article->category->name }}</b>
                 </div>
                 <p class="card-text">{{ $article->body }}</p>
                 <a class="btn btn-warning" href="{{ url("/articles/delete/$article->id") }}">
@@ -14,5 +15,24 @@
                 </a>
             </div>
         </div>
+
+        <ul class="list-group">
+            <li class="list-group-item active">
+                <b>Comments ({{ count($article->comments) }})</b>
+            </li>
+            @foreach($article->comments as $comment)
+                <li class="list-group-item">
+                    {{ $comment->content }}
+                    <a href="{{ url("/comments/delete/$comment->id") }}" class="close">&times;</a>
+                </li>
+            @endforeach
+        </ul>
+
+        <form action="{{ url('/comments/add') }}" method="post">
+            @csrf
+            <input type="hidden" name="article_id" value="{{ $article->id }}">
+            <textarea name="content" class="form-control mb-2" placeholder="New Comment"></textarea>
+            <input type="submit" value="Add Comment" class="btn btn-secondary">
+        </form>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\Product\ProductController;
 
 /*
@@ -26,4 +27,7 @@ Route::get('articles/add', [ArticleController::class, 'add']);
 Route::post('articles/add', [ArticleController::class, 'create']);
 Route::get('articles/delete/{id}', [ArticleController::class, 'delete']);
 
-Route::get('products', [ProductController::class, 'index']);
+Route::post('comments/add', [CommentController::class, 'create']);
+Route::get('comments/delete/{id}', [CommentController::class, 'delete']);
+
+Route::get('products', [ProductController::class, 'index'])->middleware('auth');
